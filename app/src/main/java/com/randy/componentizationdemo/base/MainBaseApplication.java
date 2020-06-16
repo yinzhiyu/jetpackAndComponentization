81b242544334471cd2b0322eba56d76474c7c5b8
randy.yin@ximalaya.com <aini@1234>
2020-06-16 15:36:26 +0800

    删除无用 import

--- app/src/main/java/com/randy/componentizationdemo/base/MainBaseApplication.java
+++ app/src/main/java/com/randy/componentizationdemo/base/MainBaseApplication.java
@@ -1,11 +1,6 @@
 package com.randy.componentizationdemo.base;
 
-import android.app.Application;
-
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.randy.commonlib.base.BaseApplication;
-
-import static com.randy.commonlib.BuildConfig.DEBUG;
 
 /**
  * @author randy
